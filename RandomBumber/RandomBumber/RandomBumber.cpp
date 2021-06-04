e9a50be2bbd4bc0aaf02581c76acd52df065b4d3
Victorpetts <vike.pet@hotmail.com>
2021-06-04 13:54:20 +0200

    Counts appearance per number and percentage

--- RandomBumber/RandomBumber/RandomBumber.cpp
+++ RandomBumber/RandomBumber/RandomBumber.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 using namespace std;
 
 int main(){
-	const int iterations = 100;
+	const int iterations = 1000;
 	int number;
+	int maxNumber = 20;
+	int appearances[20] = {0};
+	
 	srand(time(0));
 
 	for(int i = 0; i < iterations; i++){
-		number = rand() & 20 + 1;
-		cout << number << " ";
+		number = rand() % maxNumber;
+		appearances[number] += 1;
+		cout << number + 1 << " ";
+	}
+	cout << endl;
+
+	for(int i = 0; i < maxNumber; i++){
+		float percentage = (float(appearances[i]) / iterations) * 100;
+		cout << "Number " << i + 1 << ": " << appearances[i] << " times " << percentage << "%" << endl;
 	}
 }
